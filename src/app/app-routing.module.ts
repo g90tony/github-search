e4638fd11ffbf3d63ad423209012081c9569e3ed
Caleb Tony <calebmbugua@gmail.com>
2021-04-07 18:11:41 +0300

    added routes for searchQuery and searchResults

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { SearchComponent } from './components/search/search.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { SearchQueryComponent } from './components/search-query/search-query.component';
+import { SearchLandingComponent } from './components/search-landing/search-landing.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent,
   },
-  { path: 'search', component: SearchComponent },
+  { path: 'search', component: SearchLandingComponent },
+  { path: 'search-query', component: SearchQueryComponent },
+  { path: 'search-results', component: SearchResultsComponent },
 ];
 
 @NgModule({
