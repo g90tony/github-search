b5945bca421068736d0e400925b51874851cabc9
Caleb Tony <calebmbugua@gmail.com>
2021-04-07 13:42:56 +0300

    imported newly created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { RepoViewComponent } from './components/repo-view/repo-view.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavBarComponent,
+    SearchResultsComponent,
+    RepoViewComponent,
+    UserViewComponent,
+    SearchComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
