c187be2074a167a8f455c82d441489a21a88b8fb
Caleb Tony <calebmbugua@gmail.com>
2021-04-08 14:16:44 +0300

    added compnent

---
+++ src/app/components/user-profile/user-profile.component.ts
@@ -0,0 +1,73 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Repository } from 'src/app/classes/repository';
+import { User } from 'src/app/classes/user';
+import { RepoHttpService } from 'src/app/services/repo-http.service';
+import { UserHttpService } from 'src/app/services/user-http.service';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent implements OnInit {
+  constructor(
+    private userHttpService: UserHttpService,
+    private repoHttpService: RepoHttpService,
+    private privateUser: User,
+    private privateUserRepositories: Array<Repository>
+  ) {}
+
+  authenticatedUser: any;
+  authenticatedRepositories: any;
+
+  ngOnInit(): void {
+    this.userHttpService.getMyData().subscribe((userData: any) => {
+      this.privateUser = this.createNewInstance(userData);
+      this.authenticatedUser = this.privateUser;
+
+      if (this.authenticatedUser) {
+        this.repoHttpService.getMyRepos().subscribe((userRepos: any) => {
+          this.privateUserRepositories = this.fetchUserRepos(userRepos);
+          this.authenticatedRepositories = this.privateUserRepositories;
+        });
+      }
+    });
+  }
+
+  createNewInstance(data: any) {
+    const total_repos = data.total_private_repos + data.public_repos;
+
+    const authenticatedUser = new User(
+      data.avatar_url,
+      data.bio,
+      data.created_at,
+      data.email,
+      data.hireable,
+      data.followers,
+      data.following,
+      data.location,
+      data.name,
+      data.twitter,
+      total_repos
+    );
+
+    return authenticatedUser;
+  }
+
+  fetchUserRepos(arr: any) {
+    return arr.map((data: any) => {
+      const authenticatedRepo = new Repository(
+        data.full_name,
+        data.language,
+        data.homepage,
+        data.stargazers_count,
+        data.description,
+        data.owner.login,
+        data.bio,
+        data.html_url
+      );
+      console.log(authenticatedRepo);
+      return authenticatedRepo;
+    });
+  }
+}
