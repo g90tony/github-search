8dafa410d207f65b495cb554a6ea2846b0d02f4c
Caleb Tony <calebmbugua@gmail.com>
2021-04-08 04:54:40 +0300

    added fetch authenticated user's data request

--- src/app/services/user-http.service.ts
+++ src/app/services/user-http.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserHttpService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  ApiUrl = 'https://api.github.com';
+
+  url = `${this.ApiUrl}/user?access_token=${environment.ApiKey}`;
+
+  getMyData() {
+    return this.http.get(this.url);
+  }
 }
