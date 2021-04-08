d1f39013cac639fe61534a1dffdfe55926a0222e
Caleb Tony <calebmbugua@gmail.com>
2021-04-08 04:56:37 +0300

    added fetch user logic

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/classes/user';
+import { UserHttpService } from 'src/app/services/user-http.service';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
+  constructor(
+    private userHttpService: UserHttpService,
+    private AuthenticatedUser: User
+  ) {}
 
-  constructor() { }
+  authenticatedUser: any;
 
   ngOnInit(): void {
+    this.userHttpService.getMyData().subscribe((userData: any) => {
+      this.AuthenticatedUser = this.createNewInstance(userData);
+      this.authenticatedUser = this.AuthenticatedUser;
+      console.log(this.AuthenticatedUser);
+    });
   }
 
+  createNewInstance(data: any) {
+    const total_repos = data.total_private_repos + data.public_repos;
+
+    const authenticatedUser = new User(
+      data.avatar_url,
+      data.bio,
+      data.created_at,
+      data.email,
+      data.hireable,
+      data.followers,
+      data.following,
+      data.location,
+      data.name,
+      data.twitter,
+      total_repos
+    );
+
+    return authenticatedUser;
+  }
 }
