3fb833e45b43ae274a08710290dfc9040f1195fd
Caleb Tony <calebmbugua@gmail.com>
2021-04-08 04:55:27 +0300

    added access key for authorization

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  ApiKey: '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',
 };
 
 /*
