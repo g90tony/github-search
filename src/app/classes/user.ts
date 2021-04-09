485aa19c74ce5c8120ac0d73391856f9b219d5ab
Caleb Tony <calebmbugua@gmail.com>
2021-04-09 04:02:30 +0300

    minor class edits
    added username

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -6,12 +6,13 @@ export class User {
     public email: string,
     public followers: number,
     public following: number,
-    public location: number,
+    public location: string,
     public hireable: string,
     public name: string,
     public twitter: string,
     public total_repos: number,
-    public github_url: string
+    public github_url: string,
+    public username: string
   ) {
     this.avatar = avatar;
     this.bio = bio ? bio : 'Not Available';
@@ -20,10 +21,11 @@ export class User {
     this.hireable = hireable ? 'Yes' : 'No';
     this.followers = followers;
     this.following = following;
-    this.location = location;
+    this.location = location ? location : 'Not Available';
     this.created_at = created_at;
     this.twitter = twitter ? twitter : 'Not Available';
     this.total_repos = total_repos;
     this.github_url = github_url;
+    this.username = `@${username}`;
   }
 }
